bc67ce4293e0c3218196a59e78c1f32b840a85b4
androsida <83917283+androsida@users.noreply.github.com>
2026-04-30 14:21:42 +0200

    Update download_datasets.py

--- download_datasets.py
+++ download_datasets.py
@@ -1,7 +1,7 @@
 URLS = {
     "tokyo_xs": "https://drive.google.com/file/d/19vrphwImf7lpoaVlKgHhN07AWj_EfhjR/view?usp=drive_link",
     "sf_xs": "https://drive.google.com/file/d/1JaM1y16fArlSF0KPRqCjggMQCIRwXzJm/view?usp=drive_link",
-    "gsv_xs": "https://drive.google.com/file/d/1q7usSe9_5xV5zTfN-1In4DlmF5ReyU_A/view?usp=share_link",
+    "gsv_xs": "https://drive.google.com/file/d/12nlyYCx-VUZLB2fP8GHQDihzgCiRjpLS/view?usp=drive_link",
     "svox": "https://drive.google.com/file/d/1cpXTLfQ94IcAFQkxCnUjROTVdkwgq23a/view?usp=drive_link"
 }
 
