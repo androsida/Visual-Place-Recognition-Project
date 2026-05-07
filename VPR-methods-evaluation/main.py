4a7fe8f4bda34eb5abe41fb6b90440d8d4fc1319
androsida <83917283+androsida@users.noreply.github.com>
2026-05-07 16:43:55 +0200

    Update main.py

--- VPR-methods-evaluation/main.py
+++ VPR-methods-evaluation/main.py
@@ -99,7 +99,7 @@ def main(args):
 
     #--- INIZIO SECONDO CRONOMETRO
     distances, predictions = faiss_index.search(queries_descriptors, k)
-
+      #--- FINE SECONDO CRONOMETRO
     del database_descriptors, all_descriptors
 
     # For each query, check if the predictions are correct
@@ -111,7 +111,7 @@ def main(args):
                 if np.any(np.in1d(preds[:n], positives_per_query[query_index])):
                     recalls[i:] += 1
                     break
-  #--- FINE SECONDO CRONOMETRO
+
         # Divide by num_queries and multiply by 100, so the recalls are in percentages
         recalls = recalls / test_ds.num_queries * 100
         recalls_str = ", ".join([f"R@{val}: {rec:.1f}" for val, rec in zip(args.recall_values, recalls)])
